0ead321fe02342d8e5a8fe2f0c3d27d4ca822326
zeha <z0527111980@gmail.com>
2024-11-17 18:36:43 +0200

    gitIgnore

--- backend/EmployeeManagerAPI/obj/Debug/net8.0/EmployeeManagerAPI.AssemblyInfo.cs
+++ backend/EmployeeManagerAPI/obj/Debug/net8.0/EmployeeManagerAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("EmployeeManagerAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0f2b41a3c7f58181756eb3b9ca921b60832b2843")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c85d4b66e176b345e101aeacd93d418da923e13")]
 [assembly: System.Reflection.AssemblyProductAttribute("EmployeeManagerAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("EmployeeManagerAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
