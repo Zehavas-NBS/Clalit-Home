ae93a7c07a42177b68f2007342e6909c5be7da73
zeha <z0527111980@gmail.com>
2024-11-24 02:53:48 +0200

    chect token experetion on evert request to server

--- frontend/src/Infra/axiosInstance.ts
+++ frontend/src/Infra/axiosInstance.ts
@@ -1,14 +1,42 @@
 import axios from "axios";
+import { jwtDecode } from 'jwt-decode';
 
+
+
+interface MyTokenType {
+  sub: string;
+  name: string;
+  iat: number;
+  exp: number;
+}
 const axiosInstance = axios.create({
   baseURL: "http://localhost:5009/api", 
 });
+
+
+const isTokenExpired = (token : string) => {
+  try {
+
+      const decodedToken = jwtDecode<MyTokenType>(token);
+      const currentTime = Math.floor(Date.now() / 1000);
+      return decodedToken.exp < currentTime;
+  } catch (error) {
+      console.error("Invalid token:", error);
+      return true;
+  }
+};
 
 
 axiosInstance.interceptors.request.use(
   (config) => {
     const token = localStorage.getItem("authToken"); 
     if (token) {
+      if(isTokenExpired(token)){
+        alert('Session expired. Please log in again.');
+        localStorage.removeItem("authToken");
+        window.location.href = "/login";
+        return Promise.reject(new Error('Token expired'));
+      }
       config.headers.Authorization = `Bearer ${token}`;
       config.headers["Content-Type"] = "application/json";
     }
